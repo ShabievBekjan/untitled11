311625e8422c2767545c1ee6b94782b5a97093b2
ShabievBekjan <beka.shabiev@gmail.com>
2022-07-07 16:07:23 +0600

    исправил код

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,22 @@
 public class Main {
 
-    public static void temperatureMethod(int age, int temperature) {
-
-        if(age > 20 && age < 45 && temperature >= -20 && temperature <= 30){
-            System.out.println("You can go for a walk");
-        }
-        if(age < 20 && temperature >= 0 && temperature <= 28){
-            System.out.println("You can go for a walk");
-        }
-        if(age < 45 && temperature >= -10 && temperature <= 25){
-            System.out.println("You can go for a walk");
-        }
-        else {
-            System.out.println("Stay at home");
-        }
-    }
-
     public static void main(String[] args) {
-
         temperatureMethod(21,20);
         temperatureMethod(19,20);
         temperatureMethod(46,20);
+    }
+    public static int temperatureMethod(int age, int temperature) {
+
+        if(age > 20 && age < 45 && temperature >= -20 && temperature <= 30){
+            System.out.println("You can go for a walk");
+        }else if(age < 20 && temperature >= 0 && temperature <= 28){
+            System.out.println("You can go for a walk");
+        } else if(age < 45 && temperature >= -10 && temperature <= 25){
+            System.out.println("You can go for a walk");
+        } else {
+            System.out.println("Stay at home");
+        }
+        return age + temperature;
     }
 }
 
