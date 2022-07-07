4db845aba1bdd4bd99fca9b79865621b2e236ec9
ShabievBekjan <beka.shabiev@gmail.com>
2022-07-07 16:42:38 +0600

    исправил код и сделал дз на сообразительность

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,29 @@
+import java.util.Random;
+
 public class Main {
 
     public static void main(String[] args) {
-        temperatureMethod(21,20);
-        temperatureMethod(19,20);
-        temperatureMethod(46,20);
+        System.out.println(temperatureMethod(generteRandomAe(), 20));
+        System.out.println(temperatureMethod(19, 20));
+        System.out.println(temperatureMethod(46, 15));
+        System.out.println(temperatureMethod(21, 43));
+        System.out.println(temperatureMethod(15, 28));
     }
-    public static int temperatureMethod(int age, int temperature) {
 
-        if(age > 20 && age < 45 && temperature >= -20 && temperature <= 30){
-            System.out.println("You can go for a walk");
-        }else if(age < 20 && temperature >= 0 && temperature <= 28){
-            System.out.println("You can go for a walk");
-        } else if(age < 45 && temperature >= -10 && temperature <= 25){
-            System.out.println("You can go for a walk");
-        } else {
-            System.out.println("Stay at home");
+    public static String temperatureMethod(int age, int temperature) {
+        if (age > 20 && age < 45 && temperature >= -20 && temperature <= 30) {
+          return "You can go for a walk";
+        } else if (age < 20 && temperature >= 0 && temperature <= 28) {
+            return "You can go for a walk";
+        } else if (age < 45 && temperature >= -10 && temperature <= 25) {
+            return "You can go for a walk";
+        }else {
+            return "Stay at home";
         }
-        return age + temperature;
+    }
+    public static int generteRandomAe(){
+        Random random = new Random();
+        return random.nextInt(45);
     }
 }
 
